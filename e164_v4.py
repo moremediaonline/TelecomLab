77417dbac66cb6cc98de5c4c07c4f854840b234f
moremediaonline <hhaddadi@hotmail.com>
2026-08-02 18:01:52 +0200

    E164_v4 Validator 1error-line59

--- e164_v4.py
+++ e164_v4.py
@@ -41,10 +41,10 @@ for nummer in nummers:
         if nummer.startswith("+31"):
             print("nl Nederlands nummer")
 
-        if nummer.startswith("+316"):
-             print("[MOBILE] Nederlands mobiel nummer")
-        else:
-            print("[FIXED] Nederlands vast nummer")
+            if nummer.startswith("+316"):
+                 print("[MOBILE] Nederlands mobiel nummer")
+            else:
+                print("[FIXED] Nederlands vast nummer")
 
         elif nummer.startswith("+49"):
             print("de Duitsland")
@@ -55,8 +55,8 @@ for nummer in nummers:
         elif nummer.startswith("+33"):
             print("fr Frankrijk")
 
-       else:
-           print("[WARN] Onbekende landcode")
+        else:
+            print("[WARN] Onbekende landcode")
 
 else:
     print("[ERROR] Ongeldig  E.164 nummer")
