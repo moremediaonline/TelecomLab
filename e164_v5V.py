17cbf5feff4fe060890a796910ccf2f3a013073a
moremediaonline <hhaddadi@hotmail.com>
2026-08-08 12:14:11 +0200

    v5

---
+++ e164_v5V.py
@@ -0,0 +1,83 @@
+import re
+
+def normaliseer_nummer(nummer):
+
+    if nummer.startswith("06"):
+        nummer ="+31" + nummer[1:]
+
+    elif nummer.startswith("0031"):
+        nummer = "+" + nummer[2:]
+
+    return nummer
+
+#einde funtie
+
+nummers = [
+    "0612345678",
+    "0031612345678",
+    "+31612345678",
+    "+493012345678"
+]
+
+patroon = r"^\+[1-9][0-9]{1,14}$"
+
+
+for nummer in nummers:
+    
+
+    nummer = nummer.strip()
+    nummer = nummer.replace(" ", "")
+    nummer = normaliseer_nummer(nummer)
+
+    print()
+    print("genormaliseerd nummer:", nummer)
+
+
+# Nederlandse omzettingen
+
+    if re.match(patroon, nummer):
+        print("[OK] Geldig E.164 nummer")
+
+        if nummer.startswith("+31"):
+            print("nl Nederlands nummer")
+
+            if nummer.startswith("+316"):
+                 print("[MOBILE] Nederlands mobiel nummer")
+            else:
+                print("[FIXED] Nederlands vast nummer")
+
+        elif nummer.startswith("+49"):
+            print("de Duitsland")
+
+        elif nummer.startswith("+32"):
+            print("be Belgie")
+
+        elif nummer.startswith("+33"):
+            print("fr Frankrijk")
+
+        else:
+            print("[WARN] Onbekende landcode")
+
+    else:
+        print("[ERROR] Ongeldig  E.164 nummer")
+
+with open("nummers.txt", "r" ) as bestand:
+    for regel in bestand:
+        nummer = regel.strip()
+        print("\nControle van:", nummer)
+
+        nummer = normaliseer_nummer(nummer)
+
+        if nummer:
+            valideer_nummer(nummer) 
+
+
+
+
+
+
+
+
+
+
+
