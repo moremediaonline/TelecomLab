a9302250bab3f99814ec3628510721cac04e8ec7
moremediaonline <hhaddadi@hotmail.com>
2026-08-08 12:49:46 +0200

    synerln45

---
+++ e164_5v.py
@@ -0,0 +1,68 @@
+import re
+
+def normaliseer_nummer(nummer):
+
+    if nummer.startswith("06"):
+        nummer ="+31" + nummer[1:]
+
+    elif nummer.startswith("0031"):
+        nummer = "+" + nummer[2:]
+
+    return nummer
+
+#einde funtie
+
+
+
+patroon = r"^\+[1-9][0-9]{1,14}$"
+
+
+with open("nummers.txt", "r" ) as bestand:
+    for nummer in bestand:
+    
+
+        nummer = nummer.strip()
+        nummer = nummer.replace(" ", "")
+        nummer = normaliseer_nummer(nummer)
+
+        print()
+        print("genormaliseerd nummer:", nummer)
+
+
+# Nederlandse omzettingen
+
+        if re.match(patroon, nummer):
+            print("[OK] Geldig E.164 nummer")
+
+            if nummer.startswith("+31"):
+                print("nl Nederlands nummer")
+
+            if nummer.startswith("+316"):
+                 print("[MOBILE] Nederlands mobiel nummer")
+            else:
+                print("[FIXED] Nederlands vast nummer")
+
+            elif nummer.startswith("+49"):
+                print("de Duitsland")
+
+            elif nummer.startswith("+32"):
+                print("be Belgie")
+
+            elif nummer.startswith("+33"):
+                print("fr Frankrijk")
+
+            else:
+                print("[WARN] Onbekende landcode")
+
+    else:
+        print("[ERROR] Ongeldig  E.164 nummer")
+
+
+
+
+
+
+
+
+
+
